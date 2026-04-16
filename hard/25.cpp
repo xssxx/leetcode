fb38304f09fe37bb7fd98b00759572784d868c98
X <koonx6520@gmail.com>
2026-04-16 19:27:53 +0700

    reverse nodes in k-group

---
+++ hard/25.cpp
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+private:
+    ListNode* getKth(ListNode* curr, int k) {
+        while (curr && k > 0) {
+            curr = curr->next;
+            k--;
+        }
+        return curr;
+    }
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (!head || k == 1) return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+
+        ListNode *groupPrev = &dummy;
+        
+        while (true) {
+            ListNode *kth = getKth(groupPrev, k);
+            if (!kth) break;
+
+            ListNode *groupNext = kth->next;
+            ListNode *prev = groupNext;
+            ListNode *curr = groupPrev->next;
+
+            while (curr != groupNext) {
+                ListNode *tmp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = tmp;
+            }
+
+            ListNode *tmp = groupPrev->next;
+            groupPrev->next = kth;
+            groupPrev = tmp;
+        }
+        return dummy.next;
+    }
+};
